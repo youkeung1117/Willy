b89aba7b95ea74a7e7188d1c586c2a459a3a384b
You Kyoung Kim <youkyoungkim@Yous-MacBook-Pro.local>
2017-11-27 21:14:12 -0800

    Initial Commit

---
+++ Willy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Willy
+//
+//  Created by You Kyoung Kim on 2017. 11. 27..
+//  Copyright © 2017년 You Kyoung Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

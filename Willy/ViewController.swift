43414cd23002d0c6aeba6232cf6c0bba6f997cd7
You Kyoung Kim <youkyoungkim@Yous-MacBook-Pro.local>
2017-12-04 20:59:26 -0800

    first commit

--- Willy/ViewController.swift
+++ Willy/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
+    @IBAction func action(_ sender: Any) {
+    
+        let content = UNMutableNotificationContent()
+        content.body = "Hi, Stella! It's me Willy. I'm here for you. If you need to talk, just come to me."
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false) // 5seconds; no repeat
+        
+        let request = UNNotificationRequest(identifier: "timerDone", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, error in})
+
+        
     }
 
     override func didReceiveMemoryWarning() {
